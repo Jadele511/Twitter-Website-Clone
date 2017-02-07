2111a49dc26ae8159aae02e2d5488f94f632e013
Jade Le <baongocle1991@gmail.com>
2017-02-06 16:23:03 -0800

    first commit

---
+++ views/footer.php
@@ -0,0 +1,66 @@
+<footer class="footer">
+  <div class="container">
+    <p> &copy; Jade's website 2017</p>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="loginModalTitle">Login</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <input type="hidden" name="loginActive" id="loginActive" value="1" />
+              <div class="form-group">
+                <label for="formGroupExampleInput">Email</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Email address">
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Password</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Password">
+              </div>
+          </form>
+          </div>
+          <div class="modal-footer">
+            <a id="toggleLogin" href="#">Sign up</a>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" id= "loginSignupButton" class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script>
+
+      $("#toggleLogin").click(function() {
+          if ($("#loginActive").val() == "1") {
+
+            $("#loginActive").val("0");
+            $("#loginModalTitle").html("Sign Up");
+            $("#loginSignupButton").html("Sign Up");
+            $("#toggleLogin").html("Log In");
+
+          } else {
+
+            $("#loginActive").val("1");
+            $("#loginModalTitle").html("Log In");
+            $("#loginSignupButton").html("Log In");
+            $("#toggleLogin").html("Sign Up");
+
+          }
+
+      });
+
+    </script>
+  </body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,38 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://chanvn.com/11_twitter/styles.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="http://chanvn.com/11_twitter/">Twitter</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="?page=yourtimeline">Your timeline<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="?page=yourtweets">Your tweets</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="?page=publicprofile">Public profiles</a>
+      </li>
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-success my-2 my-sm-0"  data-toggle="modal" data-target="#myModal">Login/Sign up</button>
+    </div>
+  </div>
+</nav>
